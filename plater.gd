5dcf366888898b89c049e909bccdb9c89defe7a5
Giu <120753646+Giu-11@users.noreply.github.com>
2024-08-15 17:37:26 -0300

    🚧movimento

--- plater.gd
+++ plater.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var tile_map = $"../TileMap"
+
+var movendo = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_pressed("cima")):
@@ -13,4 +17,20 @@ func _process(delta):
 		
 		
 func move(direction: Vector2):
-	#print(direction)
+	if movendo:
+		return
+		
+	var current: Vector2i = tile_map.local_to_map(global_position)
+	
+	var target: Vector2i = Vector2i(
+		current.x + direction.x,
+		current.y + direction.y
+	)
+	prints(current, target)
+	
+	var dados: TileData = tile_map.get_cell_tile_data(0, target)
+	
+	if (not(dados.get_custom_data("anda"))):
+		return
+	
+	global_position = tile_map.map_to_local(target)
